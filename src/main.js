fc0855ddc5d6e689d9f6fec4617af29950d7f71d
AaronMadera <aaron.madera06gmail.com>
2019-10-03 10:03:30 -0500

    [ADD] Vue-bootstrap.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
