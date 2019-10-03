3d07c7c51624afc835809a753f3c8ee88d1742af
AaronMadera <aaron.madera06gmail.com>
2019-10-03 10:03:14 -0500

    [ADD] Layout.

---
+++ src/views/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Welcome</h1>
+</template>
+
+<script>
+    export default {
+        name: 'Layout',
+        data() {
+            return {
+
+            };
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style>
+</style>
